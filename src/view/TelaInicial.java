c56b5da7563ca307e882fe03e4124261fabf3ac0
Ildomar <gallegohdoma@hotmail.com>
2016-08-19 11:33:58 -0300

    Comiit de criação

---
+++ src/view/TelaInicial.java
@@ -0,0 +1,55 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+
+public class TelaInicial extends JFrame {
+
+	private JPanel contentPane;
+	private JButton btnTeste;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaInicial frame = new TelaInicial();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaInicial() {
+		initialize();
+	}
+	private void initialize() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		contentPane.add(getBtnTeste());
+	}
+
+	private JButton getBtnTeste() {
+		if (btnTeste == null) {
+			btnTeste = new JButton("Teste");
+			btnTeste.setBounds(47, 160, 89, 23);
+		}
+		return btnTeste;
+	}
+}
